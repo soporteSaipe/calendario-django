e43e6b6e7bb71dcde535734361ad8bb08ef68165
soporte <soporte@saipe.local>
2025-09-17 15:05:49 -0300

    fir port otra vez

--- calendario_reservas/wsgi.py
+++ calendario_reservas/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calendario_reservas.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calendario_reservas.settings_production')
 
 application = get_wsgi_application()
