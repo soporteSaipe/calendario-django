2533305a6fb085444ae4ce48ca94d72e7c26d286
soporte <soporte@saipe.local>
2025-11-10 11:51:08 -0300

    contraseñas mas permisivas

--- calendario_reservas/settings.py
+++ calendario_reservas/settings.py
@@ -124,20 +124,8 @@ else:
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+# Validadores de contraseña deshabilitados para permitir contraseñas más flexibles
+AUTH_PASSWORD_VALIDATORS = []
 
 
 # Internationalization
